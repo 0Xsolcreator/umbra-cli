710247d73d63a1e9007ce6a22654a8e7d67d1ee9
0Xsolcreator <shreeharan2003@gmail.com>
2026-04-10 14:57:58 +0530

    chore: :wrench: add shortenPath utility function and update exports

--- source/lib/index.ts
+++ source/lib/index.ts
@@ -1,5 +1,21 @@
-export {type CliConfig, ConfigNotFoundError, readConfig, writeConfig} from './config.js';
+export {
+	type CliConfig,
+	ConfigNotFoundError,
+	readConfig,
+	writeConfig,
+} from './config.js';
 export {getClient, setClient} from './umbra/client.js';
 export {createFileSeedStorage} from './umbra/seed-storage.js';
 export {createUtxoScanner} from './umbra/scanner.js';
 export {createSignerFromKeypairFile} from './umbra/signer.js';
+export {
+	formatError,
+	formatRegistrationError,
+	formatDepositError,
+	formatWithdrawalError,
+	formatCreateUtxoError,
+	formatFetchUtxosError,
+	formatClaimUtxoError,
+	type ErrorState,
+} from './errors.js';
+export {shortenPath} from './format.js';
